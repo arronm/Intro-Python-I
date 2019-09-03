e172856b1b24eb2d0a69b36f611283931cbc0150
Arron M <marshall.arron@gmail.com>
2019-09-03 15:30:13 -0700

    Slices assignment

--- src/07_slices.py
+++ src/07_slices.py
@@ -10,28 +10,40 @@ Use Python's slice syntax to achieve the following:
 """
 
 a = [2, 4, 1, 7, 9, 6]
+b = [2, 4, 1, 7, 9]
+
+print(b[2:-2])
 
 # Output the second element: 4:
-print()
+print(a[1])
 
 # Output the second-to-last element: 9
-print()
+print(a[-2])
 
 # Output the last three elements in the array: [7, 9, 6]
-print()
+print(a[-3:])
 
 # Output the two middle elements in the array: [1, 7]
-print()
+# Note: Technically here you would want to get the length of the list, divide, then slice
+#   but for simplicities sake, I think the exercise is asking for the following:
+print(a[2:-2])
+
+# Better to find mid element(s) of an array
+import math
+mid = math.floor(len(a) / 2)
+if (mid % 2) == 1:
+  mid -= 1
+print(a[mid:-mid])
 
 # Output every element except the first one: [4, 1, 7, 9, 6]
-print()
+print(a[1:])
 
 # Output every element except the last one: [2, 4, 1, 7, 9]
-print()
+print(a[:-1])
 
 # For string s...
 
 s = "Hello, world!"
 
 # Output just the 8th-12th characters: "world"
-print()
+print(s[7:12])
